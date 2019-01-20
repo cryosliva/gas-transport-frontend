f8da45cea8995dd2ae655c482ece4a483add99c8
Karolina <tretyakovakarolina@gmail.com>
2019-01-20 21:04:33 +0300

    build

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/../dist')));
+
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname + '/../index.html'));
+    res.sendFile(path.join(__dirname+'/../index.html'));
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
