a57f258e9ebffd72efd872818732ac1f28d7d777
Karolina <tretyakovakarolina@gmail.com>
2019-01-20 19:58:03 +0300

    fix express

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../index.html'));
+    res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
