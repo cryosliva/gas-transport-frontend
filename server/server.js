fd4edb3dbcbeea9e9ad04cc699fcf8c3747826c7
Karolina <tretyakovakarolina@gmail.com>
2019-01-20 19:43:24 +0300

    fix routing

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/api/profile', (req, res) => {
+app.get('/*', function(req, res) {
     const user = {};
 
     res.json(user);
-});
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
